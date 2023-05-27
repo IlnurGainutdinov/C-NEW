cb53510fffad9737052cdfef7be4ffada81a9664
Ilnur <gainutdinov86@list.ru>
2023-05-27 14:15:45 +0400

    Added Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(-9, 100);
+    }
+    return arr;
+}
+
+System.Console.WriteLine("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(",", array));
+
+int sum = 0;
+for(int i = 1; i < array.Length; i+=2)
+{
+    sum += array[i]; 
+}
+System.Console.WriteLine(sum);   
