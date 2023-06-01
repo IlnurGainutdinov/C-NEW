e87ae12aae8017033bd462fcc934ec3d666acd83
Ilnur <gainutdinov86@list.ru>
2023-06-02 00:23:26 +0400

    Added Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+// void GetBinary(int number)
+// {
+//     if(number == 0)
+//     {
+//         return;
+//     }
+//     GetBinary(number / 2);
+//     System.Console.Write($"{number % 2} ");
+// }
+
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// GetBinary(number);
+
+
+
+int N()
+{
+int NumberToInput = 0;
+Console.Write($"Введите число: ");
+NumberToInput = Convert.ToInt32(Console.ReadLine());
+return NumberToInput;
+}
+void PrintArray(int[] ArrayToPrint)
+{
+Console.Write("[");
+for (int i = 0; i < ArrayToPrint.Length; i++)
+{
+Console.Write($"{ArrayToPrint[i]}");
+if (i < ArrayToPrint.Length - 1)
+{
+Console.Write(" ");
+}
+}
+Console.WriteLine("]");
+}
+
+int[] DecToBinConvert(int decNumberToConvert)
+{
+int temp = decNumberToConvert;
+int binArraySize = 0;
+for (binArraySize = 0; temp > 0; binArraySize++)
+temp = temp / 2;
+int[] binArray = new int[binArraySize];
+for (int i = binArraySize - 1; i >= 0; i--)
+{
+binArray[i] = decNumberToConvert % 2;
+decNumberToConvert = decNumberToConvert / 2;
+}
+return binArray;
+}
+
+int decN = N();
+int[] binNArray = DecToBinConvert(decN);
+
+Console.WriteLine("Число {0} в двоичной системе:", decN);
+PrintArray(binNArray);
