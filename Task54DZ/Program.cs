26cfae34fe969afe72c60efe6345adbee63b6f74
Ilnur <gainutdinov86@list.ru>
2023-06-07 21:30:06 +0400

    Added Task54DZ

---
+++ Task54DZ/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillMatrixWithRandomNumber(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}" + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void RowDecrease(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int n = j+1; n < matrix.GetLength(1); n++)
+            {
+                int temp = 0;
+                if(matrix[i, j] < matrix[i, n]) 
+                {
+                    temp = matrix[i,j];
+                    matrix[i,j] = matrix[i,n];
+                    matrix[i,n] = temp;
+                }
+            }
+
+        }
+    }
+}
+
+
+System.Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+FillMatrixWithRandomNumber(matrix);
+PrintMatrix(matrix);
+RowDecrease(matrix);
+System.Console.WriteLine();
+PrintMatrix(matrix);
